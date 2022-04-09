33d0aaba29f3a9b467069fd8cce50c25cd6b3984
Paphon Sae-Wong <Paphon_work@hotmail.com>
2022-04-09 13:10:20 +0700

    login with interval pattern

--- login.js
+++ login.js
@@ -2,6 +2,28 @@ const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
 const encoder = bodyParser.urlencoded();
+const fs = require('fs');
+let file = './pass.txt';
+let readFile
+
+//read typing pattern
+fs.readFile(file, 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //console.log(data)
+    readFile = data
+})
+
+//console.log(typeof readFile);
+function stringInt (x){
+    let arr = [];
+    for (var i = 0; i < x.length; i++){
+        arr.push(parseInt(x[i]));
+    }
+    return arr;
+}
 
 const app = express();
 app.use("/assets", express.static("assets"));
@@ -28,17 +50,39 @@ app.post("/",encoder, function(req,res){
     let password = req.body.password;
     let cpm = req.body.cpm;
     let meantime = req.body.meantime;
-    let margin = 300;
-    console.log(username)
-    console.log(cpm);
-    console.log(meantime);
+    let margin = 100;
+    let list = req.body.arr;
+    let go = 0;
+    //console.log(username)
+    //console.log(cpm);
+    //console.log(meantime);
+    //console.log(list);
     connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username,password],function(error,results,fields){
         if (error) throw error;
+        //check username and password
         if (results.length > 0) {
-            let data = JSON.parse(JSON.stringify(results))
-            console.log(data[0].mean);
-            let baseMean = data[0].mean;
-            if (meantime <= baseMean + margin && meantime >= baseMean - margin){
+            let data = JSON.stringify(results)
+            let arr = list.split(",")
+            let arr2 = readFile.split(",")
+            arr.shift()
+            //console.log(arr);
+            arr = stringInt(arr);
+            //console.log(arr);
+            arr2 = stringInt(arr2);
+            //console.log(arr2);
+            for (let i = 0; i < arr.length ; i++){
+                //console.log("arr: "+arr[i]);
+                //console.log("arr2: "+arr2[i]);
+                //console.log(margin);
+                //console.log(arr2[i] + margin);
+                if(arr[i] <= arr2[i] + margin && arr[i] >= arr2[i] - margin){
+                    console.log("pass");
+                    go+=1;
+                }else{
+                    go = 0;
+                }
+            }
+            if (go == arr2.length){ // meantime <= baseMean + margin && meantime >= baseMean - margin
             console.log(username + ": login successfully");
             res.redirect("/welcome");
             }
@@ -47,11 +91,10 @@ app.post("/",encoder, function(req,res){
             }
         } else {
             res.redirect("/");
+
         } 
         res.end();
     })
-
-    
 })
 
 // when login is success
@@ -63,7 +106,6 @@ app.get("/welcome", function(req,res){
 app.get("/forbidden", function(req,res){
     res.sendFile(__dirname + "/forbidden.html");
 })
-
 
 app.get("/index", function(req,res){
     res.sendFile(__dirname + "/index.html");
